4056c505c84201bc8172ca3690b4273ab00c5a2b
Anjishnu Kumar <anjishnu.kr@gmail.com>
2015-04-30 10:00:55 +0000

    added installation script

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "This starter code uses python3: Installing dependencies and useful python libraries"
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install cherrypy
+sudo pip3 install requests
+sudo pip3 install beautifulsoup4
