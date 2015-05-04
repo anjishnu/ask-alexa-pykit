6ffeb801c1670fd96430bb2d8c4414c3b782e8ae
Anjishnu Kumar <anjishnu.kr@gmail.com>
2015-05-04 02:49:53 +0000

    built command line customizations for generate_dialog_file tool

--- scripts/generate_dialog.py
+++ scripts/generate_dialog.py
@@ -1,7 +1,10 @@
 import json
+from argparse import ArgumentParser
+import os
 
-dialog_template_fpath = "dialog_template.txt"
-intent_schema_location = "../config/intent_schema.json"
+default_dialog_template_fpath = "dialog_template.txt"
+default_intent_schema_location = "../config/intent_schema.json"
+
 empty_dialog_template=('def {0}(intent_request):\n'
 '    """\n'
 '    The default dialog to be spoken on receipt of intent {1}.\n'
@@ -10,8 +13,8 @@ empty_dialog_template=('def {0}(intent_request):\n'
 '    """\n'
 '    return create_response("Hello world!")\n')
 
-def load_intent_schema():
-    with open(intent_schema_location, 'r') as intentfile:
+def load_intent_schema(schema_location = default_intent_schema_location):
+    with open(schema_location, 'r') as intentfile:
         return json.load(intentfile)
 
 def name_dialog_func(inp_str):
@@ -43,8 +46,8 @@ def generate_intent_to_func_str(intent_map):
         out_lst+=[line]
     return "".join(out_lst)
 
-def generate_dialog_file(intent_map):
-    with open(dialog_template_fpath, 'r') as dtfile:
+def generate_dialog_file(intent_map, dialog_template_path = default_dialog_template_fpath):
+    with open(dialog_template_path, 'r') as dtfile:
         dialog_file_template = dtfile.read()
 
     #print(dialog_file_template)
@@ -53,6 +56,7 @@ def generate_dialog_file(intent_map):
     dialog_map_str = generate_intent_to_func_str(intent_map)
     #print (dialog_map_str)
     dialog_file_template = dialog_file_template.replace('{0}', dialog_map_str)
+
     #Appending the dialog map functions.
     for intent_name, dialog_fn_name in intent_map.items():
         dialog_file_template+= "\n\n"
@@ -60,13 +64,23 @@ def generate_dialog_file(intent_map):
                                                        intent_name)
     return dialog_file_template
 
-#if __name__ == "__main__":
-def main():
-    intent_schema = load_intent_schema()
-    #print ("SCHEMA:"); print(json.dumps(intent_schema, indent=4))
+def main(args):
+    intent_schema = load_intent_schema(args.intent_schema)
     intent_map = {intent['intent']: name_dialog_func(intent['intent']) 
                    for intent in intent_schema['intents']}
-    #print generate_intent_to_func_str(intent_map)
-    print (generate_dialog_file(intent_map))
-    
-main()
+    output = (generate_dialog_file(intent_map, dialog_template_path=args.dialog_template))
+    with open(args.output_file, 'w') as outfile:
+        outfile.write(output)
+    return 
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-i","--input_file", help="intent schema from which to generate output file, default = ../config/intent_schema.json", 
+                        default=default_intent_schema_location, dest="intent_schema")
+    parser.add_argument("-o", "--output_file", help = "output file location, by default it will be produced in current folder with name dialog.py", 
+                        default="sample_dialog.py", dest="output_file")
+    parser.add_argument("-dt", "--dialog_template", help = "template file from which to build the dialog file, default=dialog_template.txt, do not mess with this "
+                        "unless you know what you are doing", default = default_dialog_template_fpath, dest= "dialog_template")
+    args = parser.parse_args()
+    print (args)
+    main(args)
