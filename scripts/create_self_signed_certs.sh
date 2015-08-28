c837c8f075d900d3cfd4a9b90c58ce559e9ad755
Anjishnu Kumar <anjikum@amazon.com>
2015-08-27 17:35:00 -0700

    Version 2.0 of pykit

--- scripts/create_self_signed_certs.sh
+++ scripts/create_self_signed_certs.sh
@@ -1,3 +1,5 @@
 openssl genrsa -out private-key.pem 2048
 python3 create_ssl_cnf.py
 openssl req -new -x509 -days 365 -key private-key.pem -config configuration.cnf -out certificate.pem
+mv *.pem ../keys/
+rm configuration.cnf
--- scripts/create_ssl_cnf.py
+++ scripts/create_ssl_cnf.py
@@ -1,5 +1,4 @@
-base_cnf="""
-[req]
+base_cnf="""[req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_req
 prompt = no
@@ -23,14 +22,15 @@ DNS.1 = {DNS}
 
 def create_cnf_from_input():
     print ("Creating config file... info needed: state, city, organization name and DNS")
-    state = input("Which state are you in?\n")
-    city = input("Which city?\n")
-    org = input("What is the name of your organization?\n")
-    skill = input("What is the name of your alexa skill?\n")
-    dns = input("What is your DNS?\n")
+    state = input("> Which state are you in?\n")
+    city = input("> Which city?\n")
+    org = input("> What is the name of your organization?\n")
+    skill = input("> What is the name of your alexa skill?\n")
+    dns = input("> What is your DNS?\n")
     with open("configuration.cnf", 'w') as cnfile:
         print(base_cnf.format(state=state.strip(), city=city.strip(),
                               org=org.strip(), skill=skill.strip(), DNS=dns.strip()),
               file=cnfile)
-            
+    print("Cleaning up configuration.cnf file, moving private key and certificate to ../keys folder")
+
 create_cnf_from_input()
