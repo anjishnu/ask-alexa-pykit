fd79789a50effc211adb381c516d7ed35f138e19
Anjishnu Kumar <anjishnu.kr@gmail.com>
2015-08-28 05:44:26 +0000

    Minor improvements

--- scripts/create_self_signed_certs.sh
+++ scripts/create_self_signed_certs.sh
@@ -2,4 +2,4 @@ openssl genrsa -out private-key.pem 2048
 python3 create_ssl_cnf.py
 openssl req -new -x509 -days 365 -key private-key.pem -config configuration.cnf -out certificate.pem
 mv *.pem ../keys/
-rm configuration.cnf
+mv configuration.cnf ../keys/
