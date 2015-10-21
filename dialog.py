e68e05e1d468a72158315d19e6e9be448fe1c3b5
Anjishnu Kumar <anjikum@amazon.com>
2015-10-21 01:58:27 -0700

    simplified code and APIs for version 3.0 of ASK_PyKit

--- dialog.py
+++ dialog.py
@@ -1,6 +1,6 @@
 import json
 from collections import defaultdict 
-from lib.dialog_utils import ResponseBuilder, IntentResponseBuilder, Request
+from lib.dialog_utils import ResponseBuilder, Request
 import pkgutil
 import handlers
 import inspect
@@ -13,33 +13,24 @@ def load_intent_schema(schema_location = DEFAULT_INTENT_SCHEMA_LOCATION):
                 return json.load(intentfile)
 
             
-def set_up_handlers():
-    #If no handler is specified, backoff to default handler 
+def initialize_handlers():
+    # If no handler is specified, backoff to default handler 
     init_default_handler = lambda : handlers.default_handler
     all_handlers_map = defaultdict(init_default_handler)
     intent_handlers_map = defaultdict(init_default_handler)
-
-    #Loading intent schema 
+    # Loading intent schema 
     intent_schema = load_intent_schema()
     all_intents = {intent["intent"] : { slot["name"] : slot["type"] for slot in intent['slots'] }
                    for intent in intent_schema['intents'] }
-
-    print (json.dumps(all_intents, indent=4))    
-
     member_functions = inspect.getmembers(module, inspect.isfunction)
-
     for (name, function) in member_functions:
         if hasattr(function, 'voice_handler'): #Function has been decorated as a voice_handler
-
             if 'request_type' in function.voice_handler:
                 if function.voice_handler['request_type'] in NON_INTENT_REQUESTS: 
-                    all_handlers_map[f.voice_handler['request_type']] = function
-                    
+                    all_handlers_map[f.voice_handler['request_type']] = function                    
             elif 'intent' in function.voice_handler:
                 if function.voice_handler['intent'] in all_intents:
                     intent_handler_map[function.voice_handler['intent']] = function 
-
-    # Attach intent handler to main handlers map
     all_handlers_map['IntentRequest'] = intent_handlers_map                
     return all_handlers_map
 
@@ -60,8 +51,7 @@ def route_intent(request_json):
     This code routes requests to the appropriate handler
     """
     request = Request(request_json)    
-    handler = REGISTERED_HANDLERS[request.request_type()]
+    voice_handler = REGISTERED_HANDLERS[request.request_type()]
     if request.intent_name():
-        handler = handler[request.intent_name()]
-    return handler.get_response(request)
-
+        voice_handler = voice_handler[request.intent_name()]
+    return voice_handler(request, response_builder)
--- handlers.py
+++ handlers.py
@@ -1,37 +1,39 @@
+from lib.dialog_utils import VoiceHandler, ResponseBuilder
 """
 In this file we specify default event handlers which are then populated into the handler map using metaprogramming
 Copyright Anjishnu Kumar 2015
 
-Each VoiceHandler function receives a ResponseBuilder object as input and outputs a Response object
+Each VoiceHandler function receives a ResponseBuilder object as input and outputs a Response object 
+A response object is defined as the output of ResponseBuilder.create_response()
 """
 
-from lib.dialog_utils import VoiceHandler
+r = ResponseBuilder()
+
+def default_handler(request):
+    """ The default handler gets invoked if no handler is set for a request """
+    return r.create_response(message="Just ask")
 
 
 @VoiceHandler(request_type="LaunchRequest")
-def launch_request_handler(response_builder):
-    return response_builder.create_response(message="Hello world!")
+def launch_request_handler(request):
+    return r.create_response(message="Hello world!")
 
 
 @VoiceHandler(request_type="SessionEndedRequest")
-def session_ended_request_handler(response_builder):
-    return response_builder.create_response(message="Goodbye!")
+def session_ended_request_handler(request):
+    return r.create_response(message="Goodbye!")
 
 
 @VoiceHandler(intent='CallDadIntent')
-def call_dad_intent_handler(response_builder):
-    request = response_builder.request
+def call_dad_intent_handler(request):
 
-    #
-    # Insert arbitrary business logic here
-    # 
+    phone_number = request.get_slot_value('PhoneNo')
+    phone_number = phone_number if phone_number else "NULL"    
 
-    slots = response_builder.get_slot_map()
-    phone_number = slots['PhoneNo']
-    card = response_builder.create_card(title="CallDadIntent Activated",
-                                        subtitle=None,
-                                        content="used echo to call dad on {phone_no}"
-                                        .format(str(phone_number)))
+    card = r.create_card(title="CallDadIntent Activated",
+                         subtitle=None,
+                         content="used echo to call dad on {phone_no}"
+                         .format(str(phone_number)))
     
     return response_builder.create_response(message="Calling dad now...",
                                             end_session=False,
@@ -39,5 +41,5 @@ def call_dad_intent_handler(response_builder):
 
 
 @VoiceHandler(intent="CallBack")
-def call_back_intent_handler(response_builder):
-    return response_builder
+def call_back_intent_handler(r):
+    return r.create_response(message="Initiating CallBack")
