47ba27c0a1c9095e512d7a4c5ca7b7c595310364
Anjishnu Kumar <anjikum@amazon.com>
2016-03-20 12:58:03 -0700

    removed old directory structure and simplified APIs

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,6 @@
-from alexa.ask import voice, Request, ResponseBuilder as r
+#from alexa.ask import voice, Request, ResponseBuilder as r
+
+from ask import alexa
 
 """
 In this file we specify default event handlers which are then populated into the handler map using metaprogramming
@@ -17,41 +19,41 @@ def lambda_handler(request_obj, context={}):
     input 'request_obj' is JSON request converted into a nested python object.        
     '''
 
-    request = Request(request_obj)
-
+    metadata = {'user_name' : 'SomeRandomDude'} # add your own metadata to the request using key value pairs
+    
     ''' inject user relevant metadata into the request if you want to, here.
     
     e.g. Something like : 
-    ... request.meta_data['user_name'] = some_database.query_user_name(request.get_user_id()) 
+    ... metadata = {'user_name' : some_database.query_user_name(request.get_user_id())}
     
     Then in the handler function you can do something like -
     ... return r.create_response('Hello there {}!'.format(request.meta_data['user_name']))
     '''    
-    return voice.route_request(request)
+    return alexa.route_request(request, metadata)
 
     
-@voice.default_handler()
+@alexa.default_handler()
 def default_handler(request):
     """ The default handler gets invoked if no handler is set for a request """
-    return r.create_response(message="Just ask")
+    return alexa.create_response(message="Just ask")
 
 
-@voice.request_handler("LaunchRequest")
+@alexa.request_handler("LaunchRequest")
 def launch_request_handler(request):
     """
     Annoatate functions with @VoiceHandler so that they can be automatically mapped 
     to request types.
     Use the 'request_type' field to map them to non-intent requests
     """
-    return r.create_response(message="Hello Welcome to My Recipes!")
+    return alexa.create_response(message="Hello Welcome to My Recipes!")
 
 
-@voice.request_handler(request_type="SessionEndedRequest")
+@alexa.request_handler(request_type="SessionEndedRequest")
 def session_ended_request_handler(request):
-    return r.create_response(message="Goodbye!")
+    return alexa.create_response(message="Goodbye!")
 
 
-@voice.intent_handler(intent='GetRecipeIntent')
+@alexa.intent_handler(intent='GetRecipeIntent')
 def get_recipe_intent_handler(request):
     """
     Use the 'intent' field in the VoiceHandler to map to the respective intent.
@@ -59,24 +61,24 @@ def get_recipe_intent_handler(request):
     """
 
     # Get variables like userId, slots, intent name etc from the 'Request' object
-    ingredient = request.get_slot_value("Ingredient") 
-    ingredient = ingredient if ingredient else ""
+    ingredient = request.slots["Ingredient"] 
+    if ingredient == None:
+        return alexa.create_response("Could not find an ingredient!")
 
-    #Use ResponseBuilder object to build responses and UI cards
-    card = r.create_card(title="GetRecipeIntent activated",
-                         subtitle=None,
-                         content="asked alexa to find a recipe using {}"
-                         .format(ingredient))
+    card = alexa.create_card(title="GetRecipeIntent activated",
+                             subtitle=None,
+                             content="asked alexa to find a recipe using {}"
+                             .format(ingredient))
     
-    return r.create_response(message="Finding a recipe with the ingredient {}".format(ingredient),
+    return alexa.create_response("Finding a recipe with the ingredient {}".format(ingredient),
                              end_session=False,
                              card_obj=card)
 
 
 
-@voice.intent_handler(intent="NextRecipeIntent")
+@alexa.intent_handler(intent="NextRecipeIntent")
 def next_recipe_intent_handler(request):
     """
     You can insert arbitrary business logic code here
     """
-    return r.create_response(message="Getting Next Recipe ... 123")
+    return alexa.create_response(message="Getting Next Recipe ... 123")
