054d2763a75c27b6130a46626771e4721824463d
Anjishnu Kumar <anjikum@amazon.com>
2016-03-27 10:06:56 -0700

    removed redundant generate_intent_schema script

--- ask/config/config.py
+++ ask/config/config.py
@@ -8,28 +8,39 @@ import json
 
 # ---- Helper Functions ----
 
-def read_in(input_type, *args, **kwargs):
-    def _read_in(*args, **kwargs):
-        while True:
-            try:
-                tmp =  raw_input(*args, **kwargs)
-            except NameError:
-                tmp =  input(*args, **kwargs)
-            try:
-                return input_type(tmp)
-            except:
-                print ('Expected type', input_type)
-    return _read_in(*args, **kwargs)
-
-
+# Get path relative to the current file 
 path_relative_to_file = lambda rel_path: os.path.normpath(os.path.join(os.path.dirname(__file__), rel_path))
+
+# Load a json file as an object
 load_json_schema = lambda schema_location : json.load(open(schema_location))
 
-BUILTINS_LOCATION = path_relative_to_file('amazon_builtin_slots.tsv')
+
+def read_from_user(input_type, *args, **kwargs):
+    '''
+    Helper function to prompt user for input of a specific type 
+    e.g. float, str, int 
+    Designed to work with both python 2 and 3 
+    Yes I know this is ugly.
+    '''
+
+    def _read_in(*args, **kwargs):
+        while True:
+            try: tmp =  raw_input(*args, **kwargs)
+            except NameError: tmp =  input(*args, **kwargs)
+            try: return input_type(tmp)
+            except: print ('Expected type', input_type)
+
+    return _read_in(*args, **kwargs)
+
+# Location of AMAZON.BUILTIN slot types
+BUILTIN_SLOTS_LOCATION = path_relative_to_file(os.path.join('..', 'data', 'amazon_builtin_slots.tsv'))
 
 def load_builtin_slots():
+    '''
+    Helper function to load builtin slots from the data location
+    '''
     builtin_slots = {}
-    for index, line in enumerate(open(BUILTINS_LOCATION)):
+    for index, line in enumerate(open(BUILTIN_SLOTS_LOCATION)):
         o =  line.strip().split('\t')
         builtin_slots[index] = {'name' : o[0],
                                 'description' : o[1] } 
